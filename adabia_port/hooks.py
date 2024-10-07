6e1d68449ca0e67fca1d680282ca768fcf910bb1
Gharieb Khalefa <blacksnow.soon@gmail.com>
2024-10-07 07:45:39 +0000

    remove system home page from hooks

--- adabia_port/hooks.py
+++ adabia_port/hooks.py
@@ -68,12 +68,12 @@ fixtures = [
 # ----------
 
 # application home page (will override Website Settings)
-home_page = "login"
+# home_page = "login"
 
 # website user home page (by Role)
-role_home_page = {
-	"Role": "home_page"
-}
+# role_home_page = {
+# 	"Role": "home_page"
+# }
 
 # Generators
 # ----------
