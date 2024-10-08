ebb3ba32a9bd42fd37833e15a6ae1706a0cc1456
Gharieb Khalefa <blacksnow.soon@gmail.com>
2024-10-08 20:32:11 +0000

    add new field for the ships

--- adabia_port/customer_tech_support/doctype/customer_support_ticket/customer_support_ticket.js
+++ adabia_port/customer_tech_support/doctype/customer_support_ticket/customer_support_ticket.js
@@ -7,29 +7,33 @@ frappe.ui.form.on('Customer Support Ticket', {
         frm.disable_save();
 	    frm.add_custom_button('Save', () => {
 	        frm.save();
-             
         }).addClass("btn bg-success py-3 px-3 font-weight-bold text-white");
+		toggleDetails(frm)
 	},
 	ticket_event (frm) {
 	   // clear the procedure field after updating the event field
 	    frm.set_value('procedure_name', '');
-	   // hide unused section
-	    const ev_value = frm.doc.ticket_event;
-	    frappe.db.get_value('Ticket Event', ev_value, 'event').then((r)=> {
-	        
-	        const data = r.message.event;
-	         
-	        if (data.includes('Truck') || data.includes('Company')) {
-	            
-	            frm.set_df_property('section_break_details', 'hidden', 0);
-	        } else {
-	             frm.set_df_property('section_break_details', 'hidden', 1);
-	        }
-	    });
+		toggleDetails(frm)
 	   
 	},
 	onload (frm) {
-	    
-	    
 	}
 })
+
+const toggleDetails = (frm) => {
+	 // hide unused section
+	 const ev_value = frm.doc.ticket_event;
+	 if (!ev_value) return
+	 frappe.db.get_value('Ticket Event', ev_value, 'event').then((r)=> {
+		 
+		 const data = r.message.event;
+		  
+		 if (data.includes('Truck') || data.includes('Company') || data.includes('Ship')) {
+			 
+			 frm.set_df_property('section_break_details', 'hidden', 0);
+		 } else {
+
+			  frm.set_df_property('section_break_details', 'hidden', 1);
+		 }
+	 });
+}
