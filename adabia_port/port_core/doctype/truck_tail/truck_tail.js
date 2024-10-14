0c5561fed225697d15c1f948ebeded3bad45cd68
Gharieb Khalefa <blacksnow.soon@gmail.com>
2024-10-14 08:32:05 +0000

    fix title generation in tail form

--- adabia_port/port_core/doctype/truck_tail/truck_tail.js
+++ adabia_port/port_core/doctype/truck_tail/truck_tail.js
@@ -8,4 +8,20 @@ frappe.ui.form.on("Truck Tail", {
             frm.save();
         }).addClass("btn bg-success py-3 px-3 font-weight-bold text-white");
 	},
+	before_save(frm) {
+	    
+	    const letters = frm.doc.tail_letters;
+	    const letters_length = letters.length;
+	    
+	    if (letters_length === 1) {
+	        frappe.throw('Error in Tail letters');
+	    } else {
+	        const plat_char = [...letters].filter(l => l !== ' ');
+    	    frm.set_value('truck_letters', plat_char.join(" "));
+	    }
+	    frm.set_value('title', `${frm.doc.tail_letters} / ${frm.doc.tail_numbers}`);
+	},
+	after_save(frm) {
+	    frappe.set_route('truck');
+	}
 });
