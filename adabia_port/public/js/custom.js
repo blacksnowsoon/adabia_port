613c7195e828d8a6a1102f71121a80474fae7985
Gharieb Khalefa <blacksnow.soon@gmail.com>
2024-10-25 21:03:48 +0000

    add version to footer

--- adabia_port/public/js/custom.js
+++ adabia_port/public/js/custom.js
@@ -4,7 +4,8 @@ const main_section = document.querySelectorAll('.main-section')[0];
 const footer = main_section.querySelector('footer');
 
 const footer_content = 
-`<div class="footer-content navbar">
+`<div class="footer-content navbar container">
     <p>© 2024 GO Smart Soultion. All rights reserved. BY-<strong>Gharieb Khalefa</strong>@ISFP Built with Frappe</p>
+    <p>Adabia Port Version 0.0.1</p>
   </div>`;
 footer.innerHTML = footer_content;
