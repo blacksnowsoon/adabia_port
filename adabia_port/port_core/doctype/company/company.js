2dc27ef846af4067b1481a8224ee7c1373be23dd
Gharieb Khalefa <blacksnow.soon@gmail.com>
2024-11-15 17:44:21 +0000

    fix bug syntex error

--- adabia_port/port_core/doctype/company/company.js
+++ adabia_port/port_core/doctype/company/company.js
@@ -1,20 +1,13 @@
 // Copyright (c) 2024, Gharieb Kalefa and contributors
 // For license information, please see license.txt
 
-<<<<<<< HEAD
-frappe.ui.form.on("Company", {
-	
-});
-=======
+
 frappe.ui.form.on('Company', {
 	refresh(frm) {
-		frm.disable_save();
-	    frm.add_custom_button('Save', () => {
-            frm.save();
-        }).addClass("btn bg-success py-3 px-3 font-weight-bold text-white");
+		save_btn(frm)
 	},
 	after_save (frm) {
 	    frappe.set_route('company')
 	}
 })
->>>>>>> 5ae9972710af5b0dd4344f06c36b5662bc1f202a
+
