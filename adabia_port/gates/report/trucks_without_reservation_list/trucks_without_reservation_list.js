64c6722fdb854731c1913cb30c59c962f65ae9c3
Gharieb Khalefa <blacksnow.soon@gmail.com>
2025-02-01 20:45:48 +0000

    trucks_without_reservation_list report

--- adabia_port/gates/report/trucks_without_reservation_list/trucks_without_reservation_list.js
+++ adabia_port/gates/report/trucks_without_reservation_list/trucks_without_reservation_list.js
@@ -3,6 +3,87 @@
 
 frappe.query_reports["Trucks Without Reservation List"] = {
 	"filters": [
+    {
+      "fieldname":"from_date",
+      "label": __("From Date"),
+      "fieldtype": "Date",
+      "default": frappe.datetime.get_today(),
+      "reqd": 1
+    },
+    {
+      "fieldname":"to_date",
+      "label": __("To Date"),
+      "fieldtype": "Date",
+      "default": frappe.datetime.get_today(),
+      "reqd": 1
+    },
+    {
+      "fieldname":"truck",
+      "label": __("Truck"),
+      "fieldtype": "Link",
+      "options": "Truck",
+      "reqd": 0
+    },
+    {
+      "fieldname":"truck_tail",
+      "label": __("Truck Tail"),
+      "fieldtype": "Link",
+      "options": "Truck Tail",
+      "reqd": 0
+    },
+    {
+      "fieldname":"machine",
+      "label": __("Machine"),
+      "fieldtype": "Link",
+      "options": "Machine",
+      "reqd": 0
+    },
+    {
+      "fieldname":"status",
+      "label": __("Status"),
+      "fieldtype": "Select",
+      "options": "All\nOpen\nClosed",
+      "default": "Open",
+      "reqd": 0
+    },
+    {
+      "fieldname":"company",
+      "label": __("Company"),
+      "fieldtype": "Link",
+      "options": "Company",
+      "reqd": 0
+    }
+    // ,
+    // {
+    //   "fieldname":"count_by",
+    //   "label": __("Group By"),
+    //   "fieldtype": "Select",
+    //   "options": "Company\nTruck\nMachine",
+    //   "reqd": 0
+    // }
+	],
+  "formatter": function (value, row, column, data, default_formatter) {
+    value = default_formatter(value, row, column, data);
+    if (data) {
+      // return "<i>" + value + "</i>";
+    }
+    return value
+  },
+  "onload": function(report) {
+    if (report.report_name) { 
+      // Add footer
+      let footer = $('<div class="report-footer">')
+      .css({
+          'text-align': 'center',
+          'margin-top': '20px',
+          'font-size': '12px',
+          'color': '#888'
+      })
+      .html('By Gharieb Khalefa. © 2025 GO-Smart');
 
-	]
+    // Append footer to the report
+    $(report.page.main).append(footer);
+    }
+    
+  }
 };
--- adabia_port/gates/report/trucks_without_reservation_list/trucks_without_reservation_list.py
+++ adabia_port/gates/report/trucks_without_reservation_list/trucks_without_reservation_list.py
@@ -1,9 +1,104 @@
 # Copyright (c) 2025, Gharieb Kalefa and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 
 
 def execute(filters=None):
-	columns, data = [], []
+	columns, data = [
+		{"fieldname": "truck", "label": "رقم الشاحنة", "fieldtype": "Data", "width": 100},
+		{"fieldname": "machine", "label": "المعدة", "fieldtype": "Data", "width": 100},
+		{"fieldname": "company", "label": "الشركة", "fieldtype": "Data", "width": 150},
+		{"fieldname": "entrance_time", "label": "توقيت الدخول", "fieldtype": "Data", "width": 150},
+		{"fieldname": "checkout_time", "label": "توقيت الخروج", "fieldtype": "Data", "width": 150},
+		{"fieldname": "status", "label": "الحالة", "fieldtype": "Data", "width": 100},
+		{"fieldname": "notes", "label": "السبب", "fieldtype": "Data", "width": 200},
+		{"fieldname": "user", "label": "المستخدم", "fieldtype": "Data", "width": 100},
+	], []
+	from_date = filters.get('from_date')
+	to_date = filters.get('to_date')
+	truck = filters.get('truck')
+	machine = filters.get('machine')
+	company = filters.get('company')
+	status = filters.get('status')
+	group_by_value = filters.get('group_by')
+	params = {
+		'from_date': from_date, 'to_date': to_date
+		}
+	query = """
+		SELECT
+			truck.title as truck,
+			IF(ticket.truck_tail != '', tail.title, ' ') as tail,
+			machine.id_number as machine_id,
+			# ticket.machine as machine,
+			# ticket.truck_tail,
+			user.full_name as created_by,
+			company.company_name as company,
+			ticket.status as status,
+			CONCAT(ticket.entrance_date, " T ", TIME_FORMAT(ticket.entrance_time, '%%H:%%i')) as entrance_time,
+			CONCAT(ticket.checkout_date, " T ", TIME_FORMAT(ticket.checkout_time, "%%H:%%i")) as checkout_time,
+			ticket.notes as notes,
+			user.full_name as user
+		FROM
+			`tabTruck Without Reservation` as ticket
+			
+		LEFT JOIN `tabCompany` as company
+		ON company.name = ticket.company
+
+		LEFT JOIN `tabTruck` as truck
+		ON truck.name = ticket.truck
+
+		LEFT JOIN `tabTruck Tail` as tail
+		ON tail.name = ticket.truck_tail
+
+		LEFT JOIN `tabMachine` as machine
+		ON machine.name = ticket.machine 
+
+		LEFT JOIN `tabUser` as user
+		ON user.name = ticket.owner
+
+	"""
+	
+	if(group_by_value == "truck"):
+		query += "GROUP BY ticket.truck"
+		params['group_by_value'] = 'ticket.truck'
+	elif(group_by_value == "machine"):
+		query += "GROUP BY ticket.machine"
+		params['group_by_value'] = 'ticket.machine'
+	elif(group_by_value == "company"):
+		query += "GROUP BY ticket.company"
+		params['group_by_value'] = 'ticket.company'
+
+	
+	where = """
+		WHERE ticket.creation >= %(from_date)s
+		AND ticket.creation <= %(to_date)s 
+		"""
+	if(truck):
+		where += " AND ticket.truck = %(truck)s"
+		params['truck'] = truck
+	if(machine):
+		where += " AND ticket.machine = %(machine)s"
+		params['machine'] = machine
+	if(company):
+		where += " AND ticket.company = %(company)s"
+		params['company'] = company
+	if(status != "All"):
+		where += " AND ticket.status = %(status)s"
+		params['status'] = status
+
+	query += where
+
+	group_by = """
+		GROUP BY
+			%(group_by_value)s
+		"""
+	
+	order_by = """
+		ORDER BY
+			ticket.status ASC
+		"""
+	query += order_by
+	frappe.errprint(query)
+	data = frappe.db.sql(query, params, as_dict=1, debug=1)
 	return columns, data
