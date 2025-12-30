b238e3937a36f9f779481a89206b5798d9e9b8de
Gharieb Khalifa <blacksnow.soon@gmail.com>
2025-12-30 08:17:35 +0000

    chore: Comment out Customer Support Ticket listview settings.

--- adabia_port/customer_tech_support/doctype/customer_support_ticket/customer_support_ticket_list.js
+++ adabia_port/customer_tech_support/doctype/customer_support_ticket/customer_support_ticket_list.js
@@ -1,19 +1,19 @@
 
-frappe.listview_settings['Customer Support Ticket'] = {
-    hide_name_column: true,
-    hide_name_filter: true,
-    formatters: {
+// frappe.listview_settings['Customer Support Ticket'] = {
+//     hide_name_column: true,
+//     hide_name_filter: true,
+//     formatters: {
         
-    },
-    onload: function(listview){
+//     },
+//     onload: function(listview){
         
-    },
-    refresh: function(listview){
-        // Apply immediately on refresh
-    },
-    render_report_view: function(listview){
-        // Apply immediately on report view
-        console.log("report view rendred",listview)
-    },
+//     },
+//     refresh: function(listview){
+//         // Apply immediately on refresh
+//     },
+//     render_report_view: function(listview){
+//         // Apply immediately on report view
+//         console.log("report view rendred",listview)
+//     },
     
-};
+// };
